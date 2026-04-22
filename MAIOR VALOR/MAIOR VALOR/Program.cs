6d85b9add30c808a48c4962e4a8b01068e64af64
GBiel212 <agostinho.g@aluno.ifsp.edu.br>
2026-04-22 15:57:21 -0300

    Add files via upload

---
+++ MAIOR VALOR/MAIOR VALOR/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAIOR_VALOR
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+           
+            
+
+
+
+            Console.Write("Informe o valor 1: ");
+            int a;
+
+            a = int.Parse(Console.ReadLine());
+
+            Console.Write("Informe o valor 2: ");
+            int b;  
+            b = int.Parse(Console.ReadLine());
+
+            if (a > b)
+            {
+                Console.WriteLine("o valor 1 é maior");
+            }
+            else
+            {
+                Console.WriteLine("o valor 2 é maior");
+            }
+
+                        Console.ReadKey();  
+
+
+
+
+        }
+    }
+}
