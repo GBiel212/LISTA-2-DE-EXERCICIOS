ca4ceabedaeefbca36e0858ab682a3a5c067ecef
GBiel212 <agostinho.g@aluno.ifsp.edu.br>
2026-04-30 20:51:06 -0300

    Add files via upload

---
+++ maior valor-identicos/maior valor-identicos/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace maior_valor_identicos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Informe o valor 1: ");
+            int a;
+
+            a = int.Parse(Console.ReadLine());
+
+            Console.Write("Informe o valor 2: ");
+            int b;
+            b = int.Parse(Console.ReadLine());
+
+            if (a == b)
+            {
+                Console.WriteLine("Os valores são identicos");
+
+
+            }
+            else
+            {
+
+
+
+
+                if (a > b)
+                {
+                    Console.WriteLine("o valor 1 é maior");
+                }
+                else
+                {
+                    Console.WriteLine("o valor 2 é maior");
+                }
+            }
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
