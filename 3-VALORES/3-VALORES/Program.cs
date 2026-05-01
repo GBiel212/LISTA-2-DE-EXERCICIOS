eeea6329f61da24366be8db996d401ad1f958459
GBiel212 <agostinho.g@aluno.ifsp.edu.br>
2026-04-30 21:03:24 -0300

    Add files via upload

---
+++ 3-VALORES/3-VALORES/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_VALORES
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("DIGITE O PRIMEIRO VALOR: ");
+            double A;
+            A = double.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("DIGITE O SEGUNDO VALOR: ");
+            double B;
+            B = double.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("DIGITE O TERCEIRO VALOR: ");
+            double C;
+            C = double.Parse(Console.ReadLine());
+
+            if (A > B && A > C)
+            {
+                Console.WriteLine("MAIOR VALOR: " + A);
+            }
+            else if (B > A && B > C)
+            {
+                Console.WriteLine("MAIOR VALOR: " + B);
+            }
+            else if (C > A && C > B)
+            {
+                Console.WriteLine("MAIOR VALOR: " + C);
+            }
+           
+          
+            
+
+            Console.WriteLine("Pressione qualquer tecla para sair...");
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
