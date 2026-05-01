f7e041363bc01d201e2f1531492b33480c2630e2
GBiel212 <agostinho.g@aluno.ifsp.edu.br>
2026-04-30 21:24:04 -0300

    Add files via upload

---
+++ TERRENO GRANDE/TERRENO GRANDE/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TERRENO_GRANDE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CONSIDERE SEU TERRENO COMO UM RETANGULO PARA CALCULAR A ARÉA");
+
+
+            Console.WriteLine("Digite a base do retangulo  :");
+            Double BASE;
+            BASE = Double.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Digite a altura do retangulo :");
+            Double altura;
+            altura = Double.Parse(Console.ReadLine());
+
+            double terreno = altura * BASE;
+
+            if (terreno > 100)
+            {
+                Console.WriteLine(" A AREA DO SEU TERRENO É: " + terreno + " ---- TERRENO GRANDE ");
+            }
+            else { Console.WriteLine(" A AREA DO SEU TERRENO É: " + terreno); } 
+        }
+    }
+}
