634b356c8a325dd89287e9e1ff0d1bb89163e8e2
GBiel212 <agostinho.g@aluno.ifsp.edu.br>
2026-05-05 14:03:17 -0300

    Add files via upload

---
+++ Peso ideal/Peso ideal/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Peso_ideal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o seu peso: ");
+            double peso = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a sua altura: ");
+            double altura = double.Parse(Console.ReadLine());
+
+            double R = peso / (altura * altura);
+
+            if (R < 19)
+                Console.WriteLine("Abaixo do peso");
+            else
+                if ( 19 <= R && R < 24)
+
+                {
+                    Console.WriteLine("Peso ideal");
+                }
+                else
+                { 
+                 Console.WriteLine("Acima do peso");
+                 }
+
+
+
+
+
+
+        }
+    }
+}
