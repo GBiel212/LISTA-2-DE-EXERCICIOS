e20ae238b8e2d5120fa85c6aa30c13f6896e2d90
GBiel212 <agostinho.g@aluno.ifsp.edu.br>
2026-05-08 10:39:12 -0300

    Add files via upload

---
+++ TRIANGULO RETANGULO/TRIANGULO RETANGULO/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRIANGULO_RETANGULO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" DIGITE O VALOR DE A: ") ;
+            int a = int.Parse(Console.ReadLine());  
+
+            Console.WriteLine(" DIGITE O VALOR DE B: ") ;
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(" DIGITE O VALOR DE C: ") ;
+            int c = int.Parse(Console.ReadLine());
+
+            if (a + b > c && a + c > b && b + c > a)
+            {
+                int maior;
+                int x;
+                int y;
+
+                if (a > b && a > c)
+                {
+                    maior = a;
+                    x = b;
+                    y = c;
+                }
+                else if (b > a && b > c)
+                {
+                    maior = b;
+                    x = a;
+                    y = c;
+                }
+                else
+                {
+                    maior = c;
+                    x = a;
+                    y = b;
+                }
+
+                if (maior * maior == x * x + y * y)
+                {
+                    Console.WriteLine(" É UM TRIÂNGULO RETÂNGULO");
+                }
+                else
+                {
+                    Console.WriteLine(" NÃO É UM TRIÂNGULO RETÂNGULO");
+                }
+            }
+            else
+            {
+                Console.WriteLine(" NÃO FORMA UM TRIANGULO");
+            }
+            Console.ReadKey (); 
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
