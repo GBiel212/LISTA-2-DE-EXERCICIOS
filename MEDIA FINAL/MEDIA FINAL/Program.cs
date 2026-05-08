30c9cf229bab89cb3539069811ed6e5b0928fc47
GBiel212 <agostinho.g@aluno.ifsp.edu.br>
2026-05-08 11:26:50 -0300

    Add files via upload

---
+++ MEDIA FINAL/MEDIA FINAL/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MEDIA_FINAL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" DIGITE A NOTA DA PROVA 1: ");
+            Double nota1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(" DIGITE A NOTA DA PROVA 2: ");
+            Double nota2 = Convert.ToDouble(Console.ReadLine());
+
+            Double MEDIA = (nota1 + nota2) / 3;
+
+            if ( MEDIA >= 5 )
+            {
+                Console.WriteLine("APROVADO");
+            }
+            else
+            {
+                Console.WriteLine("REPROVADO");
+            }
+
+
+
+
+
+
+        }
+    }
+}
