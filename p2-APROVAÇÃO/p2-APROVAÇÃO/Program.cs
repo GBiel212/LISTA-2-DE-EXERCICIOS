dcebcffed02b6f18c436eb708b92a92b65272514
GBiel212 <agostinho.g@aluno.ifsp.edu.br>
2026-05-11 16:33:40 -0300

    Add files via upload

---
+++ p2-APROVAÇÃO/p2-APROVAÇÃO/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p2_APROVAÇÃO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+            double p1, p2;
+
+            Console.WriteLine("Digite a sua nota na p1: ");
+             p1 = double.Parse(Console.ReadLine());
+            
+            p2 = (15 - p1 ) / 2;
+
+            if (p2 <= 10)
+            {
+                Console.WriteLine("A sua nota na p2 deve ser no mínimo: " + p2) ;
+            }
+            else
+            {
+                Console.WriteLine("Infelizmente, não é possível alcançar a média necessária para aprovação.");  
+
+
+
+            }
+
+        }
+    }    
+}
